6c3b79c61e2b4b8dcacff95b6b49bd775ff97a95
alonsolopezjeronimo66-cpu <alonsolopezjeronimo66@gmail.com>
2025-10-22 12:36:11 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -195,7 +195,7 @@ with col2:
     st.write("Convierte audio en texto automáticamente.")
     st.markdown(
     """
-    <a href="https://texto-voz.streamlit.app/" target="_blank">
+    <a href="https://traductorjero.streamlit.app/" target="_blank">
         <button style="
             background-color:#0047b3;
             color:white;
